f0db3ef95d896d7290a4226629f13a6bcc6feb7f
RomainCestari <romain.cestari@gmail.com>
2018-08-21 20:14:53 +0200

    final commit ?

---
+++ app/assets/javascripts/creative/tab.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+
+
+ 
+
+   $(function(){
+      setInterval(function(){
+         $(".slideshow ul").animate({marginLeft:-350},800,function(){
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 3500);
+   });
